73680b50552faf8097e547cd6a6dd837bf500436
Vanessa Tolentino <vtolentino3011@gmail.com>
2026-05-13 23:12:48 +0100

    added header file

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles.css";
+
+export default function Header() {
+
+    return (
+        <div>
+            <img > </img>
+        </div>
+    );
+
+};
